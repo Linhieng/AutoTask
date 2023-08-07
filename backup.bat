62c833073ef5d549deb9dbed0d1c46bc897e64f2
linhieng <790728740@qq.com>
2023-08-07 22:37:28 +0800

    restart

--- backup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if "%1" == "hide" goto hide_window_run
-    mshta vbscript:createobject("wscript.shell").run("%~nx0 hide",0)(window.close)
-    exit
-:hide_window_run
-node C:/Users/Public/GithubCode/OneNote/backup.js
